6ed31e1e80438570861b41c6c0f6f5f669dd3454
rbohnet <rbohnet@gmail.com>
2023-11-29 07:38:17 -0700

    Initial commit

---
+++ firefox.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eu
+
+FIREFOX_PATHS=( \
+  "/Applications/Firefox.app/Contents/MacOS/firefox" \
+  "firefox"
+)
+for i in "${FIREFOX_PATHS[@]}"
+do
+  if command -v $i &> /dev/null; then
+  FIREFOX=$i
+  fi
+done
+
+
+DATA_DIR="$(mktemp -d -t 'firefox-unsafe_data_dir.XXXXXXXXXX')"
+"${FIREFOX}" \
+  -profile $DATA_DIR \
+  -no-remote \
+  -url http://localhost:8888/ \
+  >/dev/null 2>&1 &!
